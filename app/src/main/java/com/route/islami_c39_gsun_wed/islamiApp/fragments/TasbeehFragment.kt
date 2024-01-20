f969bc9a66e608ca15d8aa36705b40cb155a47d9
ZeyadShaker <zeyadshaker90@gmail.com>
2024-01-21 01:30:34 +0200

    sebha feature commit

--- app/src/main/java/com/route/islami_c39_gsun_wed/islamiApp/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islami_c39_gsun_wed/islamiApp/fragments/TasbeehFragment.kt
@@ -9,14 +9,53 @@ import com.route.islami_c39_gsun_wed.databinding.FragmentQuranBinding
 import com.route.islami_c39_gsun_wed.databinding.FragmentTasbeehBinding
 
 class TasbeehFragment : Fragment() {
+
+    lateinit var numberOfTasbeeh:String
+    lateinit var tasbeehContent:String
     lateinit var binding: FragmentTasbeehBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentTasbeehBinding.inflate(inflater)
+        binding=FragmentTasbeehBinding.inflate(inflater)
         return binding.root
     }
 
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.seb7aIc.setOnClickListener{
+            tasbeeh()
+
+        }
+    }
+    fun tasbeeh(){
+        numberOfTasbeeh = binding.valueOfTasbeeh.text.toString()
+        tasbeehContent=binding.tasbeehContent.text.toString()
+        tasbeehCounter()
+        tasbeehContent()
+
+
+    }
+
+
+    private fun tasbeehCounter() {
+        var tasbeehValu = numberOfTasbeeh.toInt()
+        tasbeehValu++
+        binding.valueOfTasbeeh.text = tasbeehValu.toString()
+    }
+    fun tasbeehContent(){
+        if (numberOfTasbeeh.equals("20")&&tasbeehContent.equals("سبحان الله")){
+            binding.tasbeehContent.text="الحمد الله"
+            binding.valueOfTasbeeh.text="0"
+        }else if (numberOfTasbeeh.equals("20")&&tasbeehContent.equals("الحمد الله")){
+            binding.tasbeehContent.text="الله اكبر"
+            binding.valueOfTasbeeh.text="0"
+        }else if (numberOfTasbeeh.equals("20")&&tasbeehContent.equals("الله اكبر")){
+            binding.tasbeehContent.text="سبحان الله"
+            binding.valueOfTasbeeh.text="0"
+
+
+
+        }
+    }}
